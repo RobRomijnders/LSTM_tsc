dc3002ec51448404b2e067709934b4993ddc6d8c
robromijnders <romijndersrob@gmail.com>
2017-01-02 13:53:30 +0100

    split model and main

--- tsc_main.py
+++ tsc_main.py
@@ -8,175 +8,85 @@ generates an output to be classified with Softmax
 """
 
 import numpy as np
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='3'
 import tensorflow as tf
+tf.logging.set_verbosity(tf.logging.ERROR)
 import matplotlib.pyplot as plt
-from tensorflow.python.framework import ops
-
-
-def sample_batch(X_train,y_train,batch_size,num_steps):
-  """ Function to sample a batch for training"""
-  N,data_len = X_train.shape
-  ind_N = np.random.choice(N,batch_size,replace=False)
-  ind_start = np.random.choice(data_len-num_steps,1)
-  X_batch = X_train[ind_N,ind_start:ind_start+num_steps]
-  y_batch = y_train[ind_N]
-  return X_batch,y_batch
-
-def check_test(X_test,y_test,batch_size,num_steps):
-  """ Function to check the test_accuracy on the entire test set"""
-  N = X_test.shape[0]
-  num_batch = np.floor(N/batch_size)
-  test_acc = np.zeros(num_batch)
-  for i in range(int(num_batch)):
-    X_batch, y_batch = sample_batch(X_test,y_test,batch_size,num_steps)
-    test_acc[i] = sess.run(accuracy,feed_dict = {input_data: X_batch, targets: y_batch,keep_prob:1})
-  return np.mean(test_acc)
-
-
-
+from tsc_model import Model,sample_batch,load_data,check_test
 
 """Hyperparamaters"""
-max_grad_norm = 5           #Clipping of the gradient before update
-num_layers = 2              #Number of stacked LSTM layers
-num_steps = 64              #Number of steps to backprop over at every batch
-hidden_size = 13            #Number of entries of the cell state of the LSTM
-max_iterations = 2000       #Maximum iterations to train
-batch_size = 30             #Batch size
-dropout = 0.8               #Keep probability of the dropout wrapper
+
+config = {}                             #Put all configuration information into the dict
+config['num_layers'] = 3                #number of layers of stacked RNN's
+config['hidden_size'] = 120              #memory cells in a layer
+config['max_grad_norm'] = 5             #maximum gradient norm during training
+config['batch_size'] = batch_size = 30  
+config['learning_rate'] = .005
+
+max_iterations = 3000
+dropout = 0.8
+ratio = np.array([0.8,0.9]) #Ratios where to split the training and validation set
 
 """Load the data"""
-dummy = True
-if dummy:
-  data_train = np.loadtxt('UCR_TS_Archive_2015/Two_Patterns/Two_Patterns_TRAIN',delimiter=',')
-  data_test_val = np.loadtxt('UCR_TS_Archive_2015/Two_Patterns/Two_Patterns_TEST',delimiter=',')
-else:
-  data_train = np.loadtxt('data_train_dummy',delimiter=',')
-  data_test_val = np.loadtxt('data_test_dummy',delimiter=',')
-data_test,data_val = np.split(data_test_val,2)
-X_train = data_train[:,1:]
-X_val = data_val[:,1:]
-X_test = data_test[:,1:]
-N = X_train.shape[0]
-Ntest = X_test.shape[0]
-# Targets have labels 1-indexed. We subtract one for 0-indexed
-y_train = data_train[:,0]-1
-y_val = data_val[:,0]-1
-y_test = data_test[:,0]-1
-num_classes = len(np.unique(y_train))
-
-
+direc = '/home/rob/Dropbox/ml_projects/LSTM/UCR_TS_Archive_2015'
+X_train,X_val,X_test,y_train,y_val,y_test = load_data(direc,ratio,dataset='ChlorineConcentration')
+N,sl = X_train.shape
+config['sl'] = sl = X_train.shape[1]
+config['num_classes'] = num_classes = len(np.unique(y_train))
 
 # Collect the costs in a numpy fashion
 epochs = np.floor(batch_size*max_iterations / N)
-print('Train with approximately %d epochs' %(epochs))
-perf_collect = np.zeros((3,int(np.floor(max_iterations /100))))
+print('Train %.0f samples in approximately %d epochs' %(N,epochs))
+perf_collect = np.zeros((4,int(np.floor(max_iterations /100))))
+
+#Instantiate a model
+model = Model(config)
 
 
-"""Place holders"""
-input_data = tf.placeholder(tf.float32, [None, num_steps], name = 'input_data')
-targets = tf.placeholder(tf.int64, [None], name='Targets')
-#Used later on for drop_out. At testtime, we pass 1.0
-keep_prob = tf.placeholder("float", name = 'Drop_out_keep_prob')
-
-with tf.name_scope("LSTM_setup") as scope:
-  cell = tf.nn.rnn_cell.LSTMCell(hidden_size)
-  cell = tf.nn.rnn_cell.DropoutWrapper(cell, output_keep_prob=keep_prob)
-  cell = tf.nn.rnn_cell.MultiRNNCell([cell] * num_layers)
-  initial_state = cell.zero_state(batch_size, tf.float32)
-  #We have only one input dimension, but we generalize our code for future expansion
-  inputs = tf.expand_dims(input_data, 2)
-
-#Define the recurrent nature of the LSTM
-with tf.name_scope("LSTM") as scope:
-  outputs = []
-  state = initial_state
-  with tf.variable_scope("LSTM_state"):
-    for time_step in range(num_steps):
-     if time_step > 0: tf.get_variable_scope().reuse_variables() #Re-use variables only after first time-step
-     (cell_output, state) = cell(inputs[:, time_step, :], state)
-     outputs.append(cell_output)       #Now cell_output is size [batch_size x hidden_size]
-  output = tf.reduce_mean(tf.pack(outputs),0)
-
-
-#Generate a classification from the last cell_output
-#Note, this is where timeseries classification differs from sequence to sequence
-#modelling. We only output to Softmax at last time step
-with tf.name_scope("Softmax") as scope:
-  with tf.variable_scope("Softmax_params"):
-    softmax_w = tf.get_variable("softmax_w", [hidden_size, num_classes])
-    softmax_b = tf.get_variable("softmax_b", [num_classes])
-  logits = tf.nn.xw_plus_b(output, softmax_w, softmax_b)
-  #Use sparse Softmax because we have mutually exclusive classes
-  loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits,targets,name = 'Sparse_softmax')
-  cost = tf.reduce_sum(loss) / batch_size
-with tf.name_scope("Evaluating_accuracy") as scope:
-  correct_prediction = tf.equal(tf.argmax(logits,1),targets)
-  accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-
-
-"""Optimizer"""
-with tf.name_scope("Optimizer") as scope:
-  tvars = tf.trainable_variables()
-  grads, _ = tf.clip_by_global_norm(tf.gradients(cost, tvars),max_grad_norm)   #We clip the gradients to prevent explosion
-  optimizer = tf.train.AdamOptimizer(5e-3)
-  gradients = zip(grads, tvars)
-  train_op = optimizer.apply_gradients(gradients)
-  # Add histograms for variables, gradients and gradient norms.
-  # The for-loop loops over all entries of the gradient and plots
-  # a histogram. We cut of
-  for gradient, variable in gradients:
-    if isinstance(gradient, ops.IndexedSlices):
-      grad_values = gradient.values
-    else:
-      grad_values = gradient
-    h1 = tf.histogram_summary(variable.name, variable)
-    h2 = tf.histogram_summary(variable.name + "/gradients", grad_values)
-    h3 = tf.histogram_summary(variable.name + "/gradient_norm", tf.global_norm([grad_values]))
-
-#Final code for the TensorBoard
-merged = tf.merge_all_summaries()
 
 """Session time"""
 sess = tf.Session() #Depending on your use, do not forget to close the session
-writer = tf.train.SummaryWriter("/home/rob/Dropbox/ml_projects/LSTM/log_tb")
-sess.run(tf.initialize_all_variables())
-
+writer = tf.summary.FileWriter("/home/rob/Dropbox/ml_projects/LSTM/log_tb", sess.graph)  #writer for Tensorboard
+sess.run(model.init_op)
 
 step = 0
 cost_train_ma = -np.log(1/float(num_classes)+1e-9)  #Moving average training cost
-for i in range(max_iterations):
-  # Calculate some sizes
-  N = X_train.shape[0]
-  #Sample batch for training
-  X_batch, y_batch = sample_batch(X_train,y_train,batch_size,num_steps)
+acc_train_ma = 0.0
+try:
+  for i in range(max_iterations):
+    X_batch, y_batch = sample_batch(X_train,y_train,batch_size)
 
-  #Next line does the actual training
-  cost_train, _ = sess.run([cost, train_op],feed_dict = {input_data: X_batch,targets: y_batch,keep_prob:dropout})
-  cost_train_ma = cost_train_ma*0.99 + cost_train*0.01
-  if i%100 == 0:
-    #Evaluate training performance
-    perf_collect[0,step] = cost_train
+    #Next line does the actual training
+    cost_train, acc_train,_ = sess.run([model.cost,model.accuracy, model.train_op],feed_dict = {model.input: X_batch,model.labels: y_batch,model.keep_prob:dropout})
+    cost_train_ma = cost_train_ma*0.99 + cost_train*0.01
+    acc_train_ma = acc_train_ma*0.99 + acc_train*0.01
+    if i%100 == 0:
+      #Evaluate training performance
+      perf_collect[0,step] = cost_train
+      perf_collect[1,step] = acc_train
 
-    #Evaluate validation performance
-    X_batch, y_batch = sample_batch(X_val,y_val,batch_size,num_steps)
-    result = sess.run([cost,merged,accuracy],feed_dict = {input_data: X_batch, targets: y_batch, keep_prob:1})
-    cost_val = result[0]
-    perf_collect[1,step] = cost_val
-    acc_val = result[2]
-    perf_collect[2,step] = acc_val
-    print('At %5.0f out of %5.0f: Cost is TRAIN%.3f(%.3f) VAL%.3f and val acc is %.3f' %(i,max_iterations,cost_train,cost_train_ma,cost_val,acc_val))
+      #Evaluate validation performance
+      X_batch, y_batch = sample_batch(X_val,y_val,batch_size)
+      cost_val, summ,acc_val = sess.run([model.cost,model.merged,model.accuracy],feed_dict = {model.input: X_batch, model.labels: y_batch, model.keep_prob:1.0})
+      perf_collect[1,step] = cost_val
+      perf_collect[2,step] = acc_val
+      print('At %5.0f/%5.0f: COST %5.3f/%5.3f(%5.3f) -- Acc %5.3f/%5.3f(%5.3f)' %(i,max_iterations,cost_train,cost_val,cost_train_ma,acc_train,acc_val,acc_train_ma))
 
+      #Write information to TensorBoard
+      writer.add_summary(summ, i)
+      writer.flush()
 
-    #Write information to TensorBoard
-    summary_str = result[1]
-    writer.add_summary(summary_str, i)
-    writer.flush()
-
-    step +=1
-acc_test = check_test(X_test,y_test,batch_size,num_steps)
+      step +=1
+except KeyboardInterrupt:
+  #Pressing ctrl-c will end training. This try-except ensures we still plot the performance
+  pass
+  
+acc_test,cost_test = check_test(model,sess,X_test,y_test)
+epoch = float(i)*batch_size/N
+print('After training %.1f epochs, test accuracy is %5.3f and test cost is %5.3f'%(epoch,acc_test,cost_test))
 
 """Additional plots"""
-print('The accuracy on the test data is %.3f' %(acc_test))
 plt.plot(perf_collect[0],label='Train')
 plt.plot(perf_collect[1],label = 'Valid')
 plt.plot(perf_collect[2],label = 'Valid accuracy')
---
+++ tsc_model.py
@@ -0,0 +1,139 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  2 12:46:39 2017
+
+@author: rob
+"""
+
+"""
+LSTM for time series classification
+Made: 30 march 2016
+
+This model takes in time series and class labels.
+The LSTM models the time series. A fully-connected layer
+generates an output to be classified with Softmax
+"""
+
+import numpy as np
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='3'
+import tensorflow as tf
+tf.logging.set_verbosity(tf.logging.ERROR)
+
+from tensorflow.python.framework import ops
+from tensorflow.python.ops import clip_ops
+
+def load_data(direc,ratio,dataset):
+  """Input:
+  direc: location of the UCR archive
+  ratio: ratio to split training and testset
+  dataset: name of the dataset in the UCR archive"""
+  datadir = direc + '/' + dataset + '/' + dataset
+  data_train = np.loadtxt(datadir+'_TRAIN',delimiter=',')
+  data_test_val = np.loadtxt(datadir+'_TEST',delimiter=',')
+  DATA = np.concatenate((data_train,data_test_val),axis=0)
+  N = DATA.shape[0]
+
+  ratio = (ratio*N).astype(np.int32)
+  ind = np.random.permutation(N)
+  X_train = DATA[ind[:ratio[0]],1:]
+  X_val = DATA[ind[ratio[0]:ratio[1]],1:]
+  X_test = DATA[ind[ratio[1]:],1:]
+  # Targets have labels 1-indexed. We subtract one for 0-indexed
+  y_train = DATA[ind[:ratio[0]],0]-1
+  y_val = DATA[ind[ratio[0]:ratio[1]],0]-1
+  y_test = DATA[ind[ratio[1]:],0]-1
+  return X_train,X_val,X_test,y_train,y_val,y_test
+
+
+def sample_batch(X_train,y_train,batch_size):
+  """ Function to sample a batch for training"""
+  N,data_len = X_train.shape
+  ind_N = np.random.choice(N,batch_size,replace=False)
+  X_batch = X_train[ind_N]
+  y_batch = y_train[ind_N]
+  return X_batch,y_batch
+  
+  
+def check_test(model,sess,X_test,y_test):
+  batch_size = model.batch_size
+  N = X_test.shape[0]
+  runs = int(np.floor(float(N)/batch_size))
+  
+  perfs = []
+  for i in xrange(runs):
+    X_batch,y_batch = sample_batch(X_test,y_test,batch_size)
+    result = sess.run([model.cost,model.accuracy],feed_dict = {model.input: X_batch, model.labels: y_batch, model.keep_prob:1.0})
+    perfs.append(tuple(result))
+  acc,cost = [np.mean(x) for x in zip(*perfs)]
+  return acc,cost
+
+class Model():
+  def __init__(self,config):
+    
+    num_layers = config['num_layers']
+    hidden_size = config['hidden_size']
+    max_grad_norm = config['max_grad_norm']
+    self.batch_size = config['batch_size']
+    sl = config['sl']
+    learning_rate = config['learning_rate']
+    num_classes = config['num_classes']
+    """Place holders"""
+    self.input = tf.placeholder(tf.float32, [None, sl], name = 'input')
+    self.labels = tf.placeholder(tf.int64, [None], name='labels')
+    self.keep_prob = tf.placeholder("float", name = 'Drop_out_keep_prob')
+
+    with tf.name_scope("LSTM_setup") as scope:
+      cell = tf.nn.rnn_cell.LSTMCell(hidden_size)
+      cell = tf.nn.rnn_cell.DropoutWrapper(cell, output_keep_prob=self.keep_prob)
+      cell = tf.nn.rnn_cell.MultiRNNCell([cell] * num_layers)
+      initial_state = cell.zero_state(self.batch_size, tf.float32)
+    
+    input_list = tf.unstack(tf.expand_dims(self.input,axis=2),axis=1)
+    outputs,_ = tf.nn.seq2seq.rnn_decoder(input_list,initial_state,cell)
+
+    output = outputs[-1]
+
+
+    #Generate a classification from the last cell_output
+    #Note, this is where timeseries classification differs from sequence to sequence
+    #modelling. We only output to Softmax at last time step
+    with tf.name_scope("Softmax") as scope:
+      with tf.variable_scope("Softmax_params"):
+        softmax_w = tf.get_variable("softmax_w", [hidden_size, num_classes])
+        softmax_b = tf.get_variable("softmax_b", [num_classes])
+      logits = tf.nn.xw_plus_b(output, softmax_w, softmax_b)
+      #Use sparse Softmax because we have mutually exclusive classes
+      loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits,self.labels,name = 'softmax')
+      self.cost = tf.reduce_sum(loss) / self.batch_size
+    with tf.name_scope("Evaluating_accuracy") as scope:
+      correct_prediction = tf.equal(tf.argmax(logits,1),self.labels)
+      self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+
+
+    """Optimizer"""
+    with tf.name_scope("Optimizer") as scope:
+      tvars = tf.trainable_variables()
+      grads, _ = tf.clip_by_global_norm(tf.gradients(self.cost, tvars),max_grad_norm)   #We clip the gradients to prevent explosion
+      optimizer = tf.train.AdamOptimizer(learning_rate)
+      gradients = zip(grads, tvars)
+      self.train_op = optimizer.apply_gradients(gradients)
+      # Add histograms for variables, gradients and gradient norms.
+      # The for-loop loops over all entries of the gradient and plots
+      # a histogram. We cut of
+      for gradient, variable in gradients:  #plot the gradient of each trainable variable
+            if isinstance(gradient, ops.IndexedSlices):
+              grad_values = gradient.values
+            else:
+              grad_values = gradient
+
+            tf.summary.histogram(variable.name, variable)
+            tf.summary.histogram(variable.name + "/gradients", grad_values)
+            tf.summary.histogram(variable.name + "/gradient_norm", clip_ops.global_norm([grad_values]))
+
+    #Final code for the TensorBoard
+    self.merged = tf.summary.merge_all()
+    self.init_op = tf.global_variables_initializer()
+
+
+
